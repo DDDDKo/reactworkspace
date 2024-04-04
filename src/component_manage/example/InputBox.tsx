89b115257e8bd74a36a3c4f1421486e8052c0195
shimtaewook <to1583@naver.com>
2024-04-04 12:49:44 +0900

    2024-04-04 12:50

---
+++ src/component_manage/example/InputBox.tsx
@@ -0,0 +1,29 @@
+/** @format */
+
+import React from "react";
+
+interface Props{
+    label : string;
+    type : 'text' | 'password';
+    placeholder : string;
+    buttonTitle : string;
+}
+
+export default function Inputbox({label, type, placeholder, buttonTitle}:Props) {
+    return (
+        <div className="input-box">
+            <div className="input-label label">{label}</div>
+            <div className="input-content-box">
+                <input
+                    className="input"
+                    type= {type}
+                    placeholder= {placeholder}
+                />
+                <div className="input-disable-button">
+                    {buttonTitle}
+                </div>
+            </div>
+            <div className="input-message"></div>
+        </div>
+    );
+}
---
+++ src/component_manage/example/SignInContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import InputBox from './InputBox'
+import './style.css';
+import './common.css';
+
+export default function SignInContainer() {
+    return (
+        <>
+            <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요.' buttonTitle='이메일 인증'/>
+        </>
+    )
+}
+
+
